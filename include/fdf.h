b2e8d6198d80052637bb04fdb3abad849a4128e1
Pleiades <113288425+alexgeiser78@users.noreply.github.com>
2023-07-20 20:31:44 +0200

    Add files via upload
    20.07.23

--- include/fdf.h
+++ include/fdf.h
@@ -95,7 +95,7 @@ typedef struct s_bresenham
 	float	ratio; // rapport entre x et y
 	t_point	current; // t_point s
 	float	diff; //valeur de e - s 
-	float	i; //mis a zero
+	float	i; //compteur
 	t_color	scl; // s color
 	t_color	ecl; // e color
 }			t_bresenham;
@@ -144,6 +144,17 @@ void	fdf_bresenham_else(t_mlx *data, t_point s, t_point e, float ratio); //
 //static void	fdf_bresenham_else_while(t_mlx *data, t_point s, t_point e, t_bresenham vr); //
 //static int	fdf_bresenham_else_while_color(t_point s, t_point e, t_bresenham vr); //
 void	fdf_print_me_all(t_mlx *data, t_mapctr *mapctr); // personal checker
+
+int			fdf_keypressed(int key, void *param);//
+int			fdf_mouseclick(int key, int x, int y, void *param);//
+void	fdf_clear_img(t_mlx *data);//
+int		fdf_parallel(t_mlx *dt);//
+int		fdf_flat(t_mlx *dt);//
+int		fdf_changecolor(t_mlx *dt);//
+void	fdf_empty_colormap(t_mlx *data, int *colors);
+
+//int	fdf_incline(int key, t_mlx *data);//
+
 ///////////////////////////////////////////
 //int line_counter(int fd, int height);
 //int ft_width(char **character);
@@ -151,19 +162,77 @@ void	fdf_print_me_all(t_mlx *data, t_mapctr *mapctr); // personal checker
 //void invalid_file_msg(int ft_return, char *argv);
 //int ft_wordcount(char *str, char c);
 
-//void	fdf_empty_colormap(t_mlx *data, int *colors);
 
-//int	fdf_parallel(t_mlx *dt);
-//int	fdf_flat(t_mlx *dt);
 
-//int	fdf_changecolor(t_mlx *dt);
 
-//void	fdf_clear_img(t_mlx *data);
+
 
 //static void	fdf_colorset_screen(t_mlx *data);
 
 //unsigned char	red_part(int color);
 //unsigned char	green_part(int color);
 //unsigned char	blue_part(int color);
+
+#endif
+
+#ifndef KEYBOARDVALUES_H
+# define KEYBOARDVALUES_H
+
+# define KV_ESCAPE 65307
+# define KV_ESCAPE_M 53
+
+# define KV_SPACE 32
+# define KV_SPACE_M 49
+
+# define KV_PLUS 61
+# define KV_PLUS_M 24
+# define KV_MINUS 45
+# define KV_MINUS_M 27
+
+# define KV_PLUS2 65451
+# define KV_MINUS2 65453
+
+# define KV_R 114
+# define KV_R_M 15
+
+# define KV_P 112
+# define KV_P_M 35
+
+# define KV_W 119
+# define KV_W_M 13
+# define KV_A 97
+# define KV_A_M 0
+# define KV_S 115
+# define KV_S_M 1
+# define KV_D 100
+# define KV_D_M 2
+
+# define KV_C 99
+# define KV_C_M 8
+
+# define KV_LEFTARROW 65361
+# define KV_LEFTARROW_M 123
+# define KV_RIGHTARROW 65363
+# define KV_RIGHTARROW_M 124
+# define KV_TOPARROW 65362
+# define KV_TOPARROW_M  126
+# define KV_BOTTOMARROW 65364
+# define KV_BOTTOMARROW_M 125
+
+# define KV_LEFTCHEVRON 44
+# define KV_LEFTCHEVRON_M 43
+# define KV_RIGHTCHEVRON 46
+# define KV_RIGHTCHEVRON_M 47
+
+/* Same on linux and macos */
+# define KV_SCROLLUP 4
+# define KV_SCROLLDOWN 5
+# define KV_SCROLLLEFT 7
+# define KV_SCROLLRIGHT 6
+
+# define KV_SCROLLBUTTON 2
+
+# define KV_LEFTCLICK 1
+# define KV_RIGHTCLICK 3
 
 #endif
