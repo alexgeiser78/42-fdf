47ff203627ba8148eda685bc882d5da710dac374
Pleiades <113288425+alexgeiser78@users.noreply.github.com>
2023-08-09 20:02:59 +0200

    Add files via upload
    09.08.23

--- include/fdf.h
+++ include/fdf.h
@@ -10,182 +10,203 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef FDF_H
 # define FDF_H
 
-#include "../minilibx/mlx.h"
-#include "../libft/libft.h"
-#include <fcntl.h> //open
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h> //hypotf
+# include "../minilibx/mlx.h"
+# include "../libft/libft.h"
+# include <fcntl.h> //open
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h> //hypotf
 
-#define FDF_WIDTH 1920
-#define FDF_HEIGHT 1080
+# define FDF_WIDTH 1920
+# define FDF_HEIGHT 1080
 
 # ifndef RAD
 #  define RAD 57.2958 //1 radian = 57.2958 deg
 # endif
 
-# ifndef mapz
-#  define mapz ((mapctr->map)[x][y])
+# ifndef MAPZ
+#  define MAPZ ((mapctr->map)[x][y])
 # endif
 
-# ifndef deg_init
-#  define deg_init 45
+# ifndef DEG_INIT
+#  define DEG_INIT 45
 # endif
 
-# ifndef incl_y
-#  define incl_y 80
+# ifndef INCL_Y
+#  define INCL_Y 80
 # endif
 
 typedef struct s_point
 {
-	float	x; //axe x
-	float	y; // axe y
-	float	z; // axe z, valeur en char
-	int		color; //valeur de la couleur en int, mais qui deviendra tripotee par brensenham if
+	float	x;
+	float	y;
+	float	z;
+	int		color;
 }		t_point;
 
 typedef struct s_mapctr
 {
-	t_point		**map;// tableau 2D axe x et y
-	int			width;// nombre de colones
-	int			height;// nombre de lignes
-	long		min; //nbr min linked to the gradient
-	long		max; //nbr max linked to the gradient
-	long		range;//  diff between max z and min z linked to gradient and color == max - min used to calculate the indice
-	long		translatex; // decalage de l'image sur l'axe x 
-	long		translatey;// decalage de l'image sur l'axe x
-	float		xorigin;// centre de rotation probable
-	float		yorigin;// centre de rotation probable
+	t_point		**map;
+	int			width;
+	int			height;
+	long		min;
+	long		max;
+	long		range;
+	long		translatex;
+	long		translatey;
+	float		xorigin;
+	float		yorigin;
 }				t_mapctr;
+//**map = tableau 2D axe x et y
+//width = nombre de colones
+//height = nombre de lignes
+//min = min z value
+//range = diff between max z and min z
+//translatex = decalage de l'image sur l'axe x
+//xorigin = centre de la map
 
 typedef struct s_mlximg
 {
-	void	*ptr; //mlx_new_image
-	char	*str; //mlx_get_data_addr 
-	int		bpp; // bit per pixel = 32 parce que 4 x 8 definie par mlx 
-	int		size_line; // longuer d'une ligne de fichier fdf = 7680, width * UNIQ_BPP definie par mlx
-	int		endian; // little endian for now on mac-intel  val = 0
+	void	*ptr;
+	char	*str;
+	int		bpp;
+	int		size_line;
+	int		endian;
 }			t_mlximg;
+//*ptr = mlx_new_image
+//*str = mlx_get_data_addr
+//bpp = bit per pixel = 32 parce que 4 x 8 defini par mlx
+//size_line = longuer d'une ligne de fichier fdf = 
+//7680, width * UNIQ_BPP defini par mlx
+//endian = little endian for now on mac-intel  val = 0
 
 typedef struct s_mlx
 {
-    void *ptr; //mlx_init 
-    void *win; //mlx_new_window
-    t_mapctr mapctr; //structure mapctr avec argument mapctr 
-    t_mlximg img; // structure mlximg avec argument image pour le dessin de l'image
-    int *colors; // pointeur sur un tableau de 256 cases de degrade de couleur
-    int nbrcolors; // nombre de degrades de couleur
-    int gradient; // permet de choisir une gamme de couleur / vecteur indiquant comment une grandeur physique varie dans l'espace, degrade de couleur
-    float scale; // valeur de l'hypothenus de height et width
-    float relief; // echelle de la hauteur y
-    float deg; // orientation de l'image (+45 deg)
-    float deg_sin; //sinus (deg / RAD) 
-    float deg_cos; // cosinus (deg / RAD)
-    float iy; // pivotement sur son axe y (80)
-    float iy_sin; // sinus (iy / RAD)
-    float iy_cos; // cosinus (iy / RAD)
-}               t_mlx;
+	void		*ptr;
+	void		*win;
+	t_mapctr	mapctr;
+	t_mlximg	img;
+	int			*colors;
+	int			nbrcolors;
+	int			gradient;
+	float		scale;
+	float		relief;
+	float		deg;
+	float		deg_sin;
+	float		deg_cos;
+	float		iy;
+	float		iy_sin;
+	float		iy_cos;
+}				t_mlx;
+//*ptr = mlx_init
+//*win = mlx_new_window
+//mapctr = structure mapctr avec argument mapctr
+//img = structure mlximg avec argument image pour le dessin de l'image
+//*colors = pointeur sur un tableau de 256 cases de degrade de couleur
+//nbrcolors = nombre de degrades de couleur
+//gradient = permet de choisir une gamme de couleur / vecteur indiquant 
+//comment une grandeur physique varie dans l'espace, degrade de couleur
+//scale = valeur de l'hypothenus de height et width
+//relief = echelle de la hauteur y
+//deg = orientation de l'image (+45 deg)
+//deg_sin = sinus (deg / RAD)
+//deg_cos = cosinus (deg / RAD)
+//iy = pivotement sur son axe y (80)
+//iy_sin = sinus (iy / RAD)
+//iy_cos = cosinus (iy / RAD)
 
 typedef struct s_color
 {
-	int				val; //
-	unsigned char	r; // rouge 
-	unsigned char	g; // vert
-	unsigned char	b; // bleu
+	int				val;
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
 }					t_color;
-
+//val = int value of color
 
 typedef struct s_bresenham
 {
-	float	ratio; // rapport entre x et y
-	t_point	current; // t_point s
-	float	diff; //valeur de e - s 
-	float	i; //compteur
-	t_color	bcol; // base color
-	t_color	ccol; // current color
+	float	ratio;
+	t_point	current;
+	float	diff;
+	float	i;
+	t_color	bcol;
+	t_color	ccol;
 }			t_bresenham;
 
-void fdf_creator(char *argv, int fd); //
-void fdf_init(t_mlx *data); //
+//ratio = rapport entre x et y
+//current = t_point s
+//diff = valeur de current - base
+//i = compteur
+//bcol = base color
+//ccol = current color
 
-void ft_exit(char *str); //
-void ft_exit_no_file(char *argv); //
-
-int fdf_fileoperations(char *argv, t_mlx *data, int fd);//
-int	fdf_set_mapsize(int fd, t_mapctr *mapctr); //
-int	fdf_set_mapsize__width(char *line); //
-int	fileoperations2(char *argv, t_mlx *data); //
-t_point	**fdf_generate_map(int fd, t_mapctr *mapctr); //
-void	fdf_map_fill_z(t_mapctr *mapctr, int fd);//
-void	fdf_free_map(t_point **map); //
-int	fdf_atoi_color(const char *str, t_mapctr *mapctr, int x, int y); //
-int	fdf_atoi_getcolor(const char *str); //
-void	fdf_findrange(t_mapctr *mapctr); //
-void	fdf_findrangee(t_mapctr *mapctr); //
-int	*fdf_colorgradient(t_mlx *data); //
-int	*fdf_colorgradient_1(t_mlx *data); //
-int	*fdf_colorgradient_2(t_mlx *data); //
-int	*fdf_colorgradient_3(t_mlx *data); //
-int	*fdf_colorgradient_4(t_mlx *data); //
-int	fdf_free_all(t_mlx *data); //
-void	fdf_colormap(t_mlx *data, int *colors); //
-long	fdf_colorgradient_indice(t_mlx *data, int x, int y); //
-int	fdf_default(void *data); //
-int	fdf_map_to_screen(t_mlx *data); //
-void	fdf_map_fill(t_mlx *data); //
-//static void	fdf_map_rotation(t_mapctr *mapctr, t_mlx *data); //
-//static void	fdf_map_relief(t_mapctr *mapctr, t_mlx *data); //
-void	fdf_fill_img(t_mlx *data); //
-int	fdf_put_pixel(t_mlx *data, int color, char *pixel); //
-t_color	rrgb(int color); //
-void	fdf_bresenham(t_mlx *data, t_point base, t_point current); //
+void	fdf_creator(char *argv, int fd);
+void	fdf_init(t_mlx *data);
+void	ft_exit(char *str);
+void	ft_exit_no_file(char *argv);
+int		fdf_fileoperations(char *argv, t_mlx *data, int fd);
+int		fdf_set_mapsize(int fd, t_mapctr *mapctr);
+int		fdf_set_mapsize__width(char *line); 
+int		fileoperations2(char *argv, t_mlx *data);
+t_point	**fdf_generate_map(int fd, t_mapctr *mapctr);
+void	fdf_map_fill_z(t_mapctr *mapctr, int fd);
+void	fdf_free_map(t_point **map);
+int		fdf_atoi_color(const char *str, t_mapctr *mapctr, int x, int y);
+int		fdf_atoi_getcolor(const char *str);
+void	fdf_findrange(t_mapctr *mapctr);
+void	fdf_findrangee(t_mapctr *mapctr);
+int		*fdf_colorgradient(t_mlx *data);
+int		*fdf_colorgradient_1(t_mlx *data);
+int		*fdf_colorgradient_2(t_mlx *data);
+int		*fdf_colorgradient_3(t_mlx *data);
+int		*fdf_colorgradient_4(t_mlx *data);
+int		fdf_free_all(t_mlx *data);
+void	fdf_colormap(t_mlx *data, int *colors);
+long	fdf_colorgradient_indice(t_mlx *data, int x, int y);
+int		fdf_default(void *data);
+int		fdf_map_to_screen(t_mlx *data);
+void	fdf_map_fill(t_mlx *data);
+//static void	fdf_map_rotation(t_mapctr *mapctr, t_mlx *data);
+//static void	fdf_map_relief(t_mapctr *mapctr, t_mlx *data);
+void	fdf_fill_img(t_mlx *data);
+int		fdf_put_pixel(t_mlx *data, int color, char *pixel);
+t_color	rrgb(int color);
+void	fdf_bresenham(t_mlx *data, t_point base, t_point current);
 void	fdf_bresenham_if(t_mlx *data, \
-t_point base, t_point current, float ratio); //
-//static void	fdf_bresenham_if_while(t_mlx *data, t_point base, t_point current, t_bresenham vr); //
-//static int	fdf_bresenham_if_while_color(t_point base, t_point current, t_bresenham vr); //
-int	rgb(unsigned char red, unsigned char green, unsigned char blue); //
-void	fdf_call_put_pixel(t_mlx *data, t_point current); //
-void	fdf_bresenham_else(t_mlx *data, t_point s, t_point e, float ratio); //
-//static void	fdf_bresenham_else_while(t_mlx *data, t_point s, t_point e, t_bresenham vr); //
-//static int	fdf_bresenham_else_while_color(t_point s, t_point e, t_bresenham vr); //
-void	fdf_print_me_all(t_mlx *data, t_mapctr *mapctr); // personal checker
-
-int			fdf_keypressed(int key, void *param);//
-int			fdf_mouseclick(int key, int x, int y, void *param);//
+t_point base, t_point current, float ratio);
+//static void	fdf_bresenham_if_while(t_mlx *data, t_point base, 
+//t_point current, t_bresenham vr);
+//static int	fdf_bresenham_if_while_color(t_point base, 
+//t_point current, t_bresenham vr);
+int		rgb(unsigned char red, unsigned char green, unsigned char blue);
+void	fdf_call_put_pixel(t_mlx *data, t_point current);
+void	fdf_bresenham_else(t_mlx *data, t_point s, t_point e, float ratio);
+//static void	fdf_bresenham_else_while(t_mlx *data, t_point s, 
+//t_point e, t_bresenham vr);
+//static int	fdf_bresenham_else_while_color(t_point s, 
+//t_point e, t_bresenham vr);
+//void	fdf_print_me_all(t_mlx *data, t_mapctr *mapctr);
+int		fdf_keypressed(int key, void *param);
+int		fdf_mouseclick(int key, int x, int y, void *param);
 void	fdf_clear_img(t_mlx *data);//
 int		fdf_parallel(t_mlx *dt);//
 int		fdf_flat(t_mlx *dt);//
 int		fdf_changecolor(t_mlx *dt);//
 void	fdf_empty_colormap(t_mlx *data, int *colors);
-
 //int	fdf_incline(int key, t_mlx *data);//
-
-///////////////////////////////////////////
-//int line_counter(int fd, int height);
-//int ft_width(char **character);
-//char **str_data(int fd, int height, char *argv);
-//void invalid_file_msg(int ft_return, char *argv);
-//int ft_wordcount(char *str, char c);
-
-
-
-
-
-
 //static void	fdf_colorset_screen(t_mlx *data);
-
 //unsigned char	red_part(int color);
 //unsigned char	green_part(int color);
 //unsigned char	blue_part(int color);
 
 #endif
 
-#ifndef KEYBOARDVALUES_H
+# ifndef KEYBOARDVALUES_H
 # define KEYBOARDVALUES_H
 
 # define KV_ESCAPE 65307
